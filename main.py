16d18d9d9bdeb17e63f6489d1e7b8b79ccccd772
IDGS801-21000381 <sunmookmp@gmail.com>
2024-02-01 00:31:54 -0600

    "AEJA 31/ENERO/2024 distancia de Puntos"

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from flask import Flask,render_template,request
+from flask import Flask, render_template, request, send_from_directory
+from forms import UserForm, PuntosForm
+import os
+import forms
 
+datos = os.urandom(24)
 
-app=Flask(__name__)
+app = Flask(__name__)
+app.secret_key = os.urandom(24)
+
 
 
 @app.route("/OperasBas")
@@ -28,8 +35,36 @@ def resul():
         else:
             return "Error: Algo hiciste mal :/ "
 
+@app.route("/puntos", methods=['GET', 'POST'])
+def puntos():
+    x1 = x2 = y1 = y2 = ''
+    puntos_clase =forms.PuntosForm (request.form)
+    if request.method == 'POST':
+        x1 = puntos_clase.x1.data
+        x2 = puntos_clase.x2.data
+        y1 = puntos_clase.y1.data
+        y2 = puntos_clase.y2.data
         
-   
+        resta1 = x2 - x1
+        resta2 = y2 - y1
+
+        distancia = (resta1 ** 2 + resta2 ** 2) ** 0.5
+
+        puntos_clase.resultado.data = distancia
+
+        print("La distancia entre los puntos es:", distancia)
+
+        print('x1: {}'.format(x1))
+        print('x2: {}'.format(x2))
+        print('y1: {}'.format(y1))
+        print('y2: {}'.format(y2))
+        print('resultado: {}'.format(distancia))
+
+    return render_template("distanciaPuntos.html", form=puntos_clase, x1=x1, x2=x2, y1=y1, y2=y2)        
+
+
+
 if __name__== "__main__":
     app.run(debug=True)
 
+app.secret_key = 'tu_datos_aqui'
